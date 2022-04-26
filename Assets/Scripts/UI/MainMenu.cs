0c3c87627dd6cbd793c1459cf5d35d5869480b83
hueyk <59758823+hueyk@users.noreply.github.com>
2022-04-26 16:21:43 -0500

    added level editor to main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -10,6 +10,10 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void Edit()
+    {
+        SceneManager.LoadScene("Level Editor");
+    }
     public void Quit()
     {
         Application.Quit();
