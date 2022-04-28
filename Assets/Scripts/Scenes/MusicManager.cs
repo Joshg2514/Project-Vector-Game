b3a316233af95f8bedec16c1e7938535cb5dace0
Jesse Chang <69780557+chaje3@users.noreply.github.com>
2022-04-27 22:56:20 -0500

    Resized tokens for all levels for balancing purposes

--- Assets/Scripts/Scenes/MusicManager.cs
+++ Assets/Scripts/Scenes/MusicManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
@@ -15,6 +16,15 @@ public class MusicManager : MonoBehaviour
             musicManagerInstance = this;
         }
         else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        Scene currentscene = SceneManager.GetActiveScene();
+        if(currentscene.name == "Main Menu")
         {
             Destroy(gameObject);
         }
